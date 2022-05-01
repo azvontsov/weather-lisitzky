7e1273459aa5ea5af732f55a993eb32699f7be68
Anton <anton@Polinas-MacBook-Air.local>
2022-05-01 12:19:38 -0400

    wip:location input worked

--- src/components/FiveDayForecast.js
+++ src/components/FiveDayForecast.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const FiveDayForecast = ({ location }) => {
-  return <div className="card-fiveday">FiveDayForecast</div>;
+  return (
+    <div className="card-fiveday">
+      <h1>Location:{location}</h1>
+    </div>
+  );
 };
 
 export default FiveDayForecast;
--- src/components/LocationSearch.js
+++ src/components/LocationSearch.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const LocationSearch = ({ location, setLocation }) => {
-  return <div>LocationSearch</div>;
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter City"
+        maxLength="50"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+      />
+      <input type="submit" value="Get weather" />
+    </div>
+  );
 };
 
 export default LocationSearch;
