89b3d148ceed20c1e4d5517cb8ebcc5f15d9ada5
Anton <anton@Polinas-MacBook-Air.local>
2022-05-01 14:15:12 -0400

    Fetch forecast

--- src/components/ForecastCard.js
+++ src/components/ForecastCard.js
@@ -3,6 +3,7 @@ import useForecast from "../hooks/useForecast";
 
 const ForecastCard = ({ date, location }) => {
   const forecast = useForecast(date, location);
+  console.log("forecast", date, location, forecast);
   return <div className="forecast-card">ForecastCard</div>;
 };
 
--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -1,6 +1,57 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import moment from "moment";
+
+const APP_ID = "c65b9d1866db1243036134173b97b27a";
+
+const api = axios.create({
+  baseURL: "http://api.openweathermap.org/",
+});
+
+async function getLatLon(location) {
+  const response = await api.get("geo/1.0/direct", {
+    params: {
+      q: location,
+      appid: APP_ID,
+    },
+  });
+  const { lat, lon } = response.data[0];
+  return { lat, lon };
+}
+
+async function getForecast(lat, lon) {
+  const response = await api.get("data/2.5/onecall", {
+    params: {
+      lat,
+      lon,
+      appid: APP_ID,
+    },
+  });
+  return response.data.daily;
+}
+
 function useForecast(date, location) {
-  const forecast = null;
-  return forecast;
+  const [forecast, setForecast] = useState(null);
+  useEffect(() => {
+    if (!location) {
+      return;
+    }
+
+    getLatLon(location)
+      .then(({ lat, lon }) => getForecast(lat, lon))
+      .then((forecast) => setForecast(forecast));
+  }, [location]);
+
+  if (!forecast) {
+    return;
+  }
+
+  const dayForecast = forecast.find((dayForecast) => {
+    const forecastTime = moment.unix(dayForecast.dt);
+    return forecastTime.dayOfYear() === date.dayOfYear();
+  });
+
+  return dayForecast;
 }
 
 export default useForecast;
